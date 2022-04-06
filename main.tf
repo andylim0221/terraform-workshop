6532b5c7ee236a7582be739dab630fd987122893
andylim0221 <limchinhuat0221@gmail.com>
2022-04-06 23:37:49 +0800

    Change tag

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "app_server" {
     instance_type = var.instance_type 
     
     tags = {
-        Name = "test"
+        Name = "Worklaod"
     }
 
     provisioner "local-exec" {
