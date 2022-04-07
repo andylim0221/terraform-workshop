c9cba83568e4a03f0e1a519b54648483e60950f4
andylim0221 <limchinhuat0221@gmail.com>
2022-04-07 18:18:33 +0800

    Why not working

--- main.tf
+++ main.tf
@@ -33,29 +33,6 @@ module "test_modules" {
   policy_name = "test-policy"
 }
 
-
-
-resource "aws_iam_policy" "second_policy" {
-  name        = "test_policy"
-  path        = "/"
-  description = "My test policy"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:Describe*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
-
 resource "aws_iam_user" "lb" {
   name = "loadbalancer"
   path = "/system/"
@@ -70,7 +47,7 @@ resource "aws_iam_access_key" "lb" {
 }
 
 resource "aws_iam_user_policy" "lb_ro" {
-  name = "test"
+  name = "dontettetetets"
   user = aws_iam_user.lb.name
 
   policy = <<EOF
